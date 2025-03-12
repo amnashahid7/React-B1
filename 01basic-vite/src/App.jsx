76ed1f5befa3ba9d188525da87b3067e61b1b5f9
Amna <naheedamna5@gmail.com>
2025-03-12 13:01:41 +0500

    react basic start

---
+++ 01basic-vite/src/App.jsx
@@ -0,0 +1,17 @@
+
+
+
+
+function App() {
+
+  return (
+    <>
+      <h1>hello world</h1>
+      <h2>ahmad</h2>
+    </>
+  )
+}
+
+export default App;
+// npx create-react-app {your application name}
+// npm create vite@latest
